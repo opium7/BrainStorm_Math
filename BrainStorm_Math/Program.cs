66251e36201ec6a2abe35f5622d9cd739f36b6af
opium7 <156327@gmail.com>
2016-05-17 20:24:53 +0300

    Add project files.

---
+++ BrainStorm_Math/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace BrainStorm
+{
+    enum Operations
+    {
+        Add,
+        Minus,
+        Multiply, 
+        Divide
+    } 
+    class Program
+    {
+        public static bool somebool;
+        public static void Main(string[] args)
+        {
+
+            /*Array from..to*/
+            int[] numbers = Enumerable.Range(3,15).ToArray();
+            /*Printing Original Array*/
+            Console.WriteLine("Original Array of numbers");
+            numbers.ArrayToString().Write();
+
+            /*Chose one ramdom number from existing array*/
+           // Console.WriteLine($"second number from Array, {numbers[2]}");
+            Console.WriteLine();
+
+            /*Random number from Array*/
+            int FirstElem = numbers[new Random().Next(0, numbers.Length)];
+            Thread.Sleep(100); //pause between randomize
+            int SecondElem = numbers[new Random().Next(0, numbers.Length)];
+
+            int Summ = FirstElem + SecondElem;
+
+
+
+            
+            Console.WriteLine(FirstElem.ToString() + "+" + SecondElem.ToString() + "=" + Summ);
+            string res = Console.ReadLine();
+            int resC;
+            bool resL = Int32.TryParse(res, out resC);
+
+            if (resL)
+            {
+                if (resC == Summ)
+                {
+                    Console.WriteLine("Correct");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect answer");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Your input " + res + " is incorrect. Try again");
+            }
+
+
+            Console.WriteLine("Correct solution is " + FirstElem.ToString() + "+" + SecondElem.ToString() + "=" + Summ);
+            Console.WriteLine();
+  
+            Console.WriteLine("I done");
+            Console.ReadKey();
+            }
+    }
+}
---
+++ RendomAndOperatorOverriding1/ExtensionsMethods.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BrainStorm_Math
+{
+    public static class ExtensionsMethods
+    {
+        public static string ArrayToString<T>(this T[] A)
+        {
+            string result = "{ ";
+            for (int i = 0; i < A.Length; i++)
+            {
+                result += $"{A[i].ToString()}, ";
+
+            }
+            result += "}";
+            return result;
+        }
+
+        //Write method
+        public static void Write(this string write)
+        {
+            Console.WriteLine(write);
+        }
+
+        //Shuffled Array with the same values
+        public static T[] ShffleArraySv<T>(this T[] A)
+        {
+            int randfromArray = 0;
+            Random random = new Random();
+            T[] result = new T[A.Length];
+            //bool[] ordered = new bool[A.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                //do
+                //{
+                    randfromArray = random.Next(A.Length);
+                //} while (ordered[randfromArray]);
+                //ordered[randfromArray] = true;
+                result[i] = A[randfromArray];
+            }
+            return result;
+        }
+
+        //Shuffled Array with constant numbers
+        public static T[] ShffleArrayCn<T>(this T[] A)
+        {
+            int randfromArray = 0;
+            Random random = new Random();
+            int constnum = 50;
+            T[] result = new T[constnum];
+            
+            //bool[] ordered = new bool[A.Length];
+
+            for (int i = 0; i < constnum; i++)
+            {
+                //do
+                //{
+                randfromArray = random.Next(A.Length);
+                //} while (ordered[randfromArray]);
+                //ordered[randfromArray] = true;
+                result[i] = A[randfromArray];
+            }
+            return result;
+        }
+
+        //Shuffled Array with constant numbers
+        // {0,1,2,3,4,5}
+        //ordered  new bool [6]
+        // 6
+        public static T[] ShffleArrayCnr<T>(this T[] A )
+        {
+            return ShffleArrayCnr(A,false);
+        }
+
+        public static T[] ShffleArrayCnr<T>(this T[] A, bool isRandom)
+        {
+            return ShffleArrayCnr(A, isRandom, A.Length);
+        }
+        public static T[] ShffleArrayCnr<T>(this T[] A, int length)
+        {
+            return ShffleArrayCnr(A, false, length);
+        }
+
+
+        public static T[] ShffleArrayCnr<T>(this T[] A,bool isRandom,int constnum)
+        {
+            int randfromArray = 0;
+            Random random = new Random();
+            T[] result = new T[constnum];
+
+            bool[] ordered = new bool[A.Length];
+
+
+            //for (int i = 0; i < result.Length; i++)
+            for (int i = 0; i < constnum; i++)
+            {
+                    do
+                    {
+                        randfromArray = random.Next(A.Length);
+                    } while (ordered[randfromArray]);
+                    ordered[randfromArray] = true;
+                result[i] = A[randfromArray];
+                
+            }
+            return result;
+        }
+
+        //Shuffled Array with all different values
+        public static T[] ShffleArray<T>(this T[] A)
+        {
+            int randfromArray = 0;
+            Random random = new Random();
+            T[] result = new T[A.Length];
+            bool[] ordered = new bool[A.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                do
+                {
+                    randfromArray = random.Next(A.Length);
+                } while (ordered[randfromArray]);
+                ordered[randfromArray] = true;
+                result[i] = A[randfromArray];
+            }
+            return result;
+        }
+
+        public static void YurasString(this string s)
+        {
+            Console.WriteLine(s + " Yuras String");
+        }
+    }
+}
